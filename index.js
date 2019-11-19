6766ab72096036559d766a8b30784f8b1d52d08e
McCoy Jamison <mccoyjamison@McCoys-MacBook-Pro.local>
2019-11-18 20:08:22 -0500

    more files

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-const express = require('express')
-const app = express()
-const teams = require('./teams.json')
-const bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+const teams = require("./teams.json");
+const bodyParser = require("body-parser");
 
-app.get('/teams', (request, response) => {
-  response.send(teams)
-})
+//todo: create the teams.js
+const teamsModel = require("./models/teams");
 
-app.get('/teams/:filter', (request, response) => {
-  let teamInfo = teams.filter( (team) => {
-    return team.id == request.params.filter || team.abbreviation == request.params.filter || team.mascot == request.params.filter
-  })
-  response.send(teamInfo)
-})
+app.get("/teams", (request, response) => {
+  response.send(teams);
+});
 
-app.post('/teams', bodyParser.json(), (request, response) => {
-  const body = request.body || {}
-  console.log({body})
-  response.send(body)
-})
+app.get("/teams/:filter", (request, response) => {
+  let teamInfo = teams.filter(team => {
+    return (
+      team.id == request.params.filter ||
+      team.abbreviation == request.params.filter ||
+      team.mascot == request.params.filter
+    );
+  });
+  response.send(teamInfo);
+});
 
-app.all('*', (request, response) => {
-  response.send('Specify a valid url please.')
-})
+app.post("/teams", bodyParser.json(), (request, response) => {
+  const body = request.body;
+  let addTeam = teams.concat(body);
+  console.log({
+    body
+  });
+  response.send(addTeam);
+});
+
+app.all("*", (request, response) => {
+  response.send("Specify a valid url please.");
+});
 app.listen(3000, () => {
-  console.log('listening on port 3000.')
-})
+  console.log("listening on port 3000.");
+});
